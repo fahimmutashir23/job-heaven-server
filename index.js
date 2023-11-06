e52666629f66685d04726ee4d2ea67946abcca9a
fahimmutashir23 <taslimit23@gmail.com>
2023-11-06 20:01:27 +0600

    added jwt verify

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookies = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 
-app.use(cors())
+app.use(cors({
+    origin : 'http://localhost:5173',
+    credentials: true
+}))
 app.use(express.json())
+app.use(cookies())
+
 
 
 
@@ -22,6 +29,22 @@ const client = new MongoClient(uri, {
   }
 });
 
+//middleWere
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if(!token){
+        return res.status(401).send({massage: 'unauthorize access'})
+    } 
+    jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+        if(err){
+            return res.status(403).send({massage : "unauthorize access"})
+        } 
+        req.user = decoded
+        next()
+    })  
+}
+
+
 async function run() {
   try {
     await client.connect();
@@ -29,19 +52,37 @@ async function run() {
     const category = client.db("jobHeaven").collection("category");
     const applyJob = client.db("jobHeaven").collection("applyJob");
 
+
+    app.post("/jwt", async(req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.SECRET_TOKEN, {expiresIn: "1hr"})
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none"
+        })
+        .send({'success': true})
+    })
+
+    app.post("/logOut", async(req, res) => {
+        res.clearCookie('token', {maxAge: 0}).send({'success': true})
+    })
+
    
     app.get("/jobs", async(req, res) => {
        let query = {};
        if(req?.query?.category){
         query = {category: req.query.category}
-       } else if(req.query.email){
+       }
+        else if(req.query.email){
         query = {email: req.query.email}
        }
         const result = await jobs.find(query).toArray();
         res.send(result)
     })
-
-    app.get("/jobs/:id", async(req, res)=> {
+    
+   
+    app.get("/jobs/:id", async(req, res)=> { 
         const id = req.params.id;
         const query = {_id: new ObjectId(id)}
         const result = await jobs.findOne(query)
@@ -50,6 +91,19 @@ async function run() {
 
     app.get("/category", async(req, res) => {
         const result = await category.find().toArray()
+        res.send(result)
+    })
+
+    app.get("/applyJob", verifyToken, async(req, res) => {
+
+        if(req?.user?.email !== req?.query?.email) {
+            return res.status(401).send({massage: 'unauthorize User'})
+        }
+        let query = {};
+        if(req?.query?.email){
+            query = {email: req?.query?.email}
+        }
+        const result = await applyJob.find(query).toArray()
         res.send(result)
     })
 
